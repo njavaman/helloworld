53bbe9778d8703561ed407f861458966778b3af8
njavaman <98298608+njavaman@users.noreply.github.com>
2023-03-02 03:09:13 -0500

    Add files via upload

---
+++ assignment7.java
@@ -0,0 +1,12 @@
+package folder;
+
+public class assignment7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
